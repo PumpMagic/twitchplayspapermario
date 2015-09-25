8b7bf547736f182d7aa7a3ccd7cf20f21a06b134
Ryan Conway <theconmethod@gmail.com>
2015-09-24 22:11:43 -0700

    Add Gamecube controller to virtc

--- src/demc/virtc/mod.rs
+++ src/demc/virtc/mod.rs
@@ -11,6 +11,25 @@ use std::collections::HashMap;
 
 trait IsVJoyDevice {
     fn get_vjoy_device_number(&self) -> u32;
+
+    // Err(1): vJoy isn't enabled
+    // Err(2): Unable to claim vjoystick
+    // Err(3): unable to reset vjoystick
+    fn claim_and_reset(&self) -> Result<(), u8> {
+        if vjoy_rust::is_vjoy_enabled() == false {
+            return Err(1);
+        }
+
+        match vjoy_rust::claim_vjoystick(self.get_vjoy_device_number()) {
+            Err(msg) => return Err(2),
+            _ => ()
+        }
+
+        match vjoy_rust::reset_vjoystick(self.get_vjoy_device_number()) {
+            Err(_) => return Err(3),
+            _ => Ok(())
+        }
+    }
 }
 
 trait HasAxes: IsVJoyDevice {
@@ -52,6 +71,16 @@ trait HasAxes: IsVJoyDevice {
             Err(_) => Err("Unable to set axis")
         }
     }
+
+    fn verify_vjoystick_axis_compatibility(&self) -> Result<(), ()> {
+        for (_, &(axis_index, _, _)) in self.get_axis_map() {
+            if vjoy_rust::get_vjoystick_axis_exists(self.get_vjoy_device_number(), axis_index)== false {
+                return Err(());
+            }
+        }
+
+        Ok(())
+    }
 }
 
 trait HasButtons: IsVJoyDevice {
@@ -76,9 +105,37 @@ trait HasButtons: IsVJoyDevice {
             Err(_) => Err("Unable to set virtual joystick button")
         }
     }
+
+    fn verify_vjoystick_button_compatibility(&self) -> Result<(), ()>
+    {
+        if (vjoy_rust::get_vjoystick_button_count(self.get_vjoy_device_number()) as usize) < self.get_button_map().len() {
+            return Err(());
+        }
+
+        Ok(())
+    }
 }
 
-pub trait AcceptsInputs: HasAxes + HasButtons {
+trait HasAxesAndButtons: HasAxes + HasButtons {
+    fn verify_vjoystick_compatibility(&self) -> Result<(), ()> {
+        match self.verify_vjoystick_axis_compatibility() {
+            Ok(_) => (),
+            Err(_) => return Err(())
+        }
+
+        match self.verify_vjoystick_button_compatibility() {
+            Ok(_) => Ok(()),
+            Err(_) => Err(())
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum Input {
+    Axis(String, f32),
+    Button(String, bool)
+}
+pub trait AcceptsInputs: HasAxesAndButtons {
     fn set_input(&self, input: &Input) -> Result<(), &'static str> {
         match input.clone() {
             Input::Axis(name, strength) => self.set_axis_state(&name, strength),
@@ -92,7 +149,6 @@ pub trait AcceptsInputs: HasAxes + HasButtons {
 pub struct VN64C {
     axes: HashMap<String, (u32, i64, i64)>,
     buttons: HashMap<String, u8>,
-
     vjoy_device_number: u32
 }
 
@@ -122,40 +178,31 @@ impl HasButtons for VN64C {
     }
 }
 
-impl AcceptsInputs for VN64C {
-}
+impl HasAxesAndButtons for VN64C {}
+
+impl AcceptsInputs for VN64C {}
 
 impl VN64C {
-    pub fn new(vjoy_device_number: u32) -> Result<Self, String> {
-        let vjoy_device_number_native = vjoy_device_number;
-
-        if vjoy_rust::is_vjoy_enabled() == false {
-            return Err(format!("vJoy isn't enabled. Have you installed vJoy?"));
-        }
-
-        match verify_vjoystick_hardware(vjoy_device_number_native) {
-            Ok(()) => (),
-            Err(err) => return Err(format!("Virtual joystick {} can't act as an N64 controller: {}.\
-                                            You may need to reconfigure your vJoy device",
-                                           vjoy_device_number, err))
-        }
-
-        match vjoy_rust::claim_vjoystick(vjoy_device_number_native) {
-            Err(msg) => return Err(format!("{}", msg)),
-            _ => ()
-        }
-
-        match vjoy_rust::reset_vjoystick(vjoy_device_number_native) {
-            Err(_) => return Err(format!("Unable to reset joystick")),
-            _ => ()
-        }
-
+    // Err(1): unable to get N64 hardware
+    // Err(2): vjoystick doesn't meet N64 controller requirements
+    // Err(3): unable to claim and reset vjoystick
+    pub fn new(vjoy_device_number: u32) -> Result<Self, u8> {
         let (axes, buttons) = match get_n64_controller_hardware(vjoy_device_number) {
             Ok((axes, buttons)) => (axes, buttons),
-            Err(_) => return Err(format!("Getting N64 controller hardware failed"))
+            Err(_) => return Err(1)
         };
 
-        Ok( VN64C { axes: axes, buttons: buttons, vjoy_device_number: vjoy_device_number } )
+        let vn64c = VN64C { axes: axes, buttons: buttons, vjoy_device_number: vjoy_device_number };
+
+        match vn64c.verify_vjoystick_compatibility() {
+            Ok(_) => (),
+            Err(_) => return Err(2)
+        }
+
+        match vn64c.claim_and_reset() {
+            Ok(_) => Ok(vn64c),
+            Err(_) => Err(3)
+        }
     }
 }
 
@@ -201,13 +248,111 @@ fn get_n64_controller_hardware(vjoy_device_number: u32) -> Result<(HashMap<Strin
     Ok((axes, buttons))
 }
 
-/*
-fn get_gcn_controller_hardware() -> ControllerInputs {
+
+//@todo track state
+pub struct VGCNC {
+    axes: HashMap<String, (u32, i64, i64)>,
+    buttons: HashMap<String, u8>,
+    vjoy_device_number: u32
+}
+
+impl IsVJoyDevice for VGCNC {
+    fn get_vjoy_device_number(&self) -> u32 {
+        return self.vjoy_device_number;
+    }
+}
+
+impl HasAxes for VGCNC {
+    fn get_axis_map(&self) -> &HashMap<String, (u32, i64, i64)> {
+        return &self.axes;
+    }
+
+    fn get_axis_state(&self, name: &String) -> Option<i64> {
+        return Some(0); //@todo
+    }
+}
+
+impl HasButtons for VGCNC {
+    fn get_button_map(&self) -> &HashMap<String, u8> {
+        return &self.buttons;
+    }
+
+    fn get_button_state(&self, name: &String) -> Option<bool> {
+        return Some(false);
+    }
+}
+
+impl HasAxesAndButtons for VGCNC {}
+
+impl AcceptsInputs for VGCNC {}
+
+impl VGCNC {
+    // Err(1): unable to get GCN hardware
+    // Err(2): vjoystick doesn't meet GCN controller requirements
+    // Err(3): unable to claim and reset vjoystick
+    pub fn new(vjoy_device_number: u32) -> Result<Self, u8> {
+        let (axes, buttons) = match get_gcn_controller_hardware(vjoy_device_number) {
+            Ok((axes, buttons)) => (axes, buttons),
+            Err(_) => return Err(1)
+        };
+
+        let vgcnc = VGCNC { axes: axes, buttons: buttons, vjoy_device_number: vjoy_device_number };
+
+        match vgcnc.verify_vjoystick_compatibility() {
+            Ok(_) => (),
+            Err(_) => return Err(2)
+        }
+
+        match vgcnc.claim_and_reset() {
+            Ok(_) => Ok(vgcnc),
+            Err(_) => Err(3)
+        }
+    }
+}
+
+fn get_gcn_controller_hardware(vjoy_device_number: u32)
+        -> Result<(HashMap<String, (u32, i64, i64)>, HashMap<String, u8>), u8>
+{
     let mut axes = HashMap::new();
-    axes.insert(String::from("jx"), 0x30); // USB HID: X
-    axes.insert(String::from("jy"), 0x31); // USB HID: Y
-    axes.insert(String::from("cx"), 0x33); // USB HID: Rx
-    axes.insert(String::from("cy"), 0x34); // USB HID: Ry
+
+    let jx_min = match vjoy_rust::get_vjoystick_axis_min(vjoy_device_number, 0x30) {
+        Ok(min) => min,
+        Err(msg) => return Err(1)
+    };
+    let jx_max = match vjoy_rust::get_vjoystick_axis_max(vjoy_device_number, 0x30) {
+        Ok(max) => max,
+        Err(_) => return Err(2)
+    };
+    let jy_min = match vjoy_rust::get_vjoystick_axis_min(vjoy_device_number, 0x31) {
+        Ok(min) => min,
+        Err(msg) => return Err(3)
+    };
+    let jy_max = match vjoy_rust::get_vjoystick_axis_max(vjoy_device_number, 0x31) {
+        Ok(max) => max,
+        Err(_) => return Err(4)
+    };
+
+    let cx_min = match vjoy_rust::get_vjoystick_axis_min(vjoy_device_number, 0x33) {
+        Ok(min) => min,
+        Err(msg) => return Err(1)
+    };
+    let cx_max = match vjoy_rust::get_vjoystick_axis_max(vjoy_device_number, 0x33) {
+        Ok(max) => max,
+        Err(_) => return Err(2)
+    };
+    let cy_min = match vjoy_rust::get_vjoystick_axis_min(vjoy_device_number, 0x34) {
+        Ok(min) => min,
+        Err(msg) => return Err(3)
+    };
+    let cy_max = match vjoy_rust::get_vjoystick_axis_max(vjoy_device_number, 0x34) {
+        Ok(max) => max,
+        Err(_) => return Err(4)
+    };
+
+    axes.insert(String::from("jx"), (0x30, jx_min, jx_max));
+    axes.insert(String::from("jy"), (0x31, jy_min, jy_max));
+    axes.insert(String::from("cx"), (0x33, cx_min, cx_max));
+    axes.insert(String::from("cy"), (0x34, cy_min, cy_max));
 
     let mut buttons = HashMap::new();
     buttons.insert(String::from("a"), 0x01);
@@ -222,30 +367,5 @@ fn get_gcn_controller_hardware() -> ControllerInputs {
     buttons.insert(String::from("dleft"), 0x0a);
     buttons.insert(String::from("dright"), 0x0b);
 
-    ControllerInputs { axes: axes, buttons: buttons }
-}
-*/
-
-#[derive(Clone)]
-pub enum Input {
-    Axis(String, f32),
-    Button(String, bool)
-}
-
-
-//@todo implement
-fn verify_vjoystick_hardware(index: u32) -> Result<(), String> {
-    if vjoy_rust::get_vjoystick_axis_exists(index, 0x30)== false {
-        return Err(format!("No X axis"));
-    }
-
-    if vjoy_rust::get_vjoystick_axis_exists(index, 0x31)== false {
-        return Err(format!("No Y axis"));
-    }
-
-    if vjoy_rust::get_vjoystick_button_count(index) < 14 {
-        return Err(format!("Less than {} buttons", 14));
-    }
-
-    Ok(())
+    Ok((axes, buttons))
 }
