3c9983e167832376c099cdfabdbdaf6c74e09849
Ryan Conway <theconmethod@gmail.com>
2015-10-09 15:31:23 -0700

    Add mod commands to unplug and plug in controller. Clean up main

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ extern crate regex;
 extern crate toml;
 extern crate time;
 
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Read;
 use std::io::Write;
@@ -21,21 +22,97 @@ use demc::vgcnc::{VGcnC, sample_gcn_controller_hardware};
 const CONFIG_FILE_PATH: &'static str = "tppm.toml";
 const CHAT_LOG_PATH: &'static str = "chat.txt";
 const VJOY_DEVICE_NUMBER: u32 = 1;
+const ACCEPT_CONTROLLER_COMMANDS_ON_BOOT: bool = true;
 
 
-
-fn handle_mod_commands(sender: &String, msg: &String) {
+enum ModCommand {
+    SaveState,
+    LoadState,
+    UnplugController,
+    PlugController
+}
+fn parse_mod_commands(sender: &String, msg: &String) -> Option<ModCommand> {
     match sender.to_lowercase().as_ref() {
         "twitchplayspapermario"|"xxn1"|"kalarmar" => {
             match msg.to_lowercase().as_ref() {
-                "!savestate" => keystroke::send_combo(&[keystroke::Key::Physical(keystroke::Physical::Shift), keystroke::Key::Physical(keystroke::Physical::F1)]),
-                "!loadstate" => keystroke::press_key(keystroke::Key::Physical(keystroke::Physical::F1)),
-                _ => ()
+                "!savestate" => Some(ModCommand::SaveState),
+                "!loadstate" => Some(ModCommand::LoadState),
+                "!unplugcontroller" => Some(ModCommand::UnplugController),
+                "!plugcontroller" => Some(ModCommand::PlugController),
+                _ => None
             }
         },
-        _ => ()
+        _ => None
     }
 }
+
+
+enum ChatMessageHandler {
+    ModCommandHandler,
+    ControllerCommandHandler,
+}
+fn log_tmi_message(sender: &String, message: &String, handler: &Option<ChatMessageHandler>, log: &mut File) {
+    let log_string = match handler {
+        &Some(ref handler) => match handler {
+            &ChatMessageHandler::ModCommandHandler => format!("!{}: {}", sender, message),
+            &ChatMessageHandler::ControllerCommandHandler => format!("_{}: {}", sender, message),
+        },
+        &None => format!("{}: {}", sender, message)
+    };
+
+    log.write_all(&log_string.as_bytes());
+    log.write_all("\r\n".as_bytes());
+    log.flush();
+    println!("{}", log_string);
+}
+
+fn handle_tmi_message<T>(sender: &String, message: &String, accepting_controller_commands: bool,
+                         controller: &DemC<T>, log: &mut File) -> Option<bool>
+{
+    let mut message_handler = None;
+    let mut new_accept_controller_command_value = None;
+    
+    if !message_handler.is_some() {
+        match parse_mod_commands(sender, message) {
+            Some(mod_command) => {
+                match mod_command {
+                    ModCommand::SaveState => {
+                        keystroke::send_combo(&[keystroke::Key::Physical(keystroke::Physical::Shift), keystroke::Key::Physical(keystroke::Physical::F1)]);
+                    },
+                    ModCommand::LoadState => {
+                        keystroke::press_key(keystroke::Key::Physical(keystroke::Physical::F1));
+                    },
+                    ModCommand::UnplugController => {
+                        new_accept_controller_command_value = Some(false);
+                    },
+                    ModCommand::PlugController => {
+                        new_accept_controller_command_value = Some(true);
+                    }
+                }
+                message_handler = Some(ChatMessageHandler::ModCommandHandler);
+            },
+            None => ()
+        }
+    }
+
+    if !message_handler.is_some() {
+        match accepting_controller_commands {
+            true => match controller.handle_commands(message) {
+                Ok(_) => {
+                    message_handler = Some(ChatMessageHandler::ControllerCommandHandler);
+                },
+                Err(_) => ()
+            },
+            false => ()
+        };
+    }
+    
+    log_tmi_message(sender, message, &message_handler, log);
+    
+    new_accept_controller_command_value
+}
+
+
 
 
 fn main() {
@@ -64,22 +141,17 @@ fn main() {
         Ok(file) => file,
         Err(reason) => panic!("Couldn't open chat log file for writing! {}", std::error::Error::description(&reason))
     };
+    
+    let mut accepting_controller_commands = ACCEPT_CONTROLLER_COMMANDS_ON_BOOT;
 
     // Poll the IRC connection and handle its messages forever
     loop {
         match tmi_stream.receive() {
             Ok((sender, message)) => {
-                let log_string = match controller.handle_commands(&message) {
-                    Ok(_) => format!("_{}: {}", sender, message),
-                    Err(_) => format!("{}: {}", sender, message)
-                };
-                
-                handle_mod_commands(&sender, &message);
-
-                chat_log_file.write_all(&log_string.as_bytes());
-                chat_log_file.write_all("\r\n".as_bytes());
-                chat_log_file.flush();
-                println!("{}", log_string);
+                match handle_tmi_message(&sender, &message, accepting_controller_commands, &controller, &mut chat_log_file) {
+                    Some(val) => { accepting_controller_commands = val; },
+                    None => ()
+                }
             },
             Err(_) => ()
         }
